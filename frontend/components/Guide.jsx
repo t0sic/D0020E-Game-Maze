e1533fdeee0e7bd6379176b524eb24ccb244cb8a
Advokado1337 <Advokado1337>
2023-12-04 15:48:05 +0100

    created guide page

--- frontend/components/Guide.jsx
+++ frontend/components/Guide.jsx
@@ -1,7 +1,25 @@
 import React from "react"
 
 const Guide = () => {
-    return <div>Guide</div>
+    return (
+        <div>
+            <div className="textbox">
+                <div className="header">Guide</div>
+                <div className="text">
+                    LOREN IPSUM
+                    TETEJSTJSKFJSDLKFJSLKDFJSLKDJFLSKJDFLKSJDFLKJSDLKFJSLKDJFLSKJDFLKSJDKLFJ
+                </div>
+                <div className="picture"></div>
+            </div>
+            <div className="textbox">
+                <div className="header">Controlls</div>
+                <div className="text">
+                    KSDJFLKSJDFLKSJDFLKJSDLFKJSDLKFJSLKDFJSLKDJFLSKJDFLKSJDF
+                </div>
+                <div className="picture"></div>
+            </div>
+        </div>
+    )
 }
 
 export default Guide
