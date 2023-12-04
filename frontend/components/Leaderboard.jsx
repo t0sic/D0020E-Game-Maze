aadf58268392dd2a99508a86261850326c9c52a8
juunte <100770493+juunte@users.noreply.github.com>
2023-12-04 15:46:28 +0100

    Created leaderboard button and page

---
+++ frontend/components/Leaderboard.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Leaderboard = ({ setPath }) => {
+    return (
+        <div className="leaderboard">
+            <div>
+                <p>Jonathan är bäst!:D</p>
+            </div>
+        </div>
+    )
+}
+
+export default Leaderboard
