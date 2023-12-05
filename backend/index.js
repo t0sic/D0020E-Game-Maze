d139e21bbc14ca8d8d42b5b9781814356bd7a5c1
Fredrik Sundh <101127364+FredrikSundh@users.noreply.github.com>
2023-12-05 14:37:53 +0100

    Implementerat webserver, tilldelning av användar id via uuid och ätter uidpå användarens cookies

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,10 @@
-import config from "../config.json" assert { type: "json" }
-import express from "express"
 import dotenv from "dotenv"
+import Webserver from "./webserver/webserver.js"
 
 // Load environment variables for development
 if (process.env.NODE_ENV !== "production") {
     dotenv.config()
 }
 
-const app = express() // Create webserver
-const PORT = process.env["PORT"] || 3000 // Define port
-
-app.use(express.static("public")) // Create static routes
-
-// Start listening on port for connections
-app.listen(PORT, () => {
-    console.log("Started webserver on port:", PORT)
-})
+const webserver = new Webserver(process.env.PORT || 3000)
+webserver.start()
---
+++ backend/webserver/webserver.js
@@ -0,0 +1,53 @@
+import express from "express"
+import cookieParser from "cookie-parser"
+import { v4 as uuid } from "uuid"
+
+export default class Webserver {
+    constructor(port) {
+        this.port = port
+        this.app = express()
+    }
+
+    start() {
+        this.middleware()
+        this.routes()
+        this.app.listen(this.port, () => {
+            console.log("Started Webserver on port:", this.port)
+        })
+    }
+
+    middleware() {
+        this.app.use(express.static("public"))
+        this.app.use(cookieParser())
+        this.app.use(this.setUser)
+    }
+
+    setUser(req, res, next) {
+        let userId = req.cookies.userId
+
+        if (!userId) {
+            userId = uuid()
+            res.cookie("userId", userId)
+        }
+        req.user = userId
+        next()
+    }
+
+    routes() {
+        console.log("hello")
+        this.app.get("/test", (req, res) => {
+            console.log(req.user)
+            res.sendStatus(200)
+        })
+    }
+}
+
+// //const app = express() // Create webserver
+// //const PORT = process.env["PORT"] || 3000 // Define port
+
+// app.use(express.static("public")) // Create static routes
+
+// // Start listening on port for connections
+// app.listen(PORT, () => {
+//     console.log("Started webserver on port:", PORT)
+// })
