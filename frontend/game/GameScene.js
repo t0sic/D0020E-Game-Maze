f1d78044145214fd68bba8c8c5ca3f119297611e
Fredrik Sundh <101127364+FredrikSundh@users.noreply.github.com>
2024-01-16 21:46:51 +0100

    Added FPS counter but for some reason scrollfactor(0) bugs out and removes the counter, Used eventemitter to give acces to the game object to GameScene, will also be used to pass the websocketroom to GameScene

--- frontend/game/GameScene.js
+++ frontend/game/GameScene.js
@@ -5,6 +5,15 @@ import Phaser from "phaser"
 class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: "GameScene" })
+
+
+        eventEmitter.on('emitGameObject', (data) => {
+            const game = data
+            console.log("recieved " + game)
+        })
+        eventEmitter.emit("sceneCreated")
+
+
     }
 
     preload = () => {
@@ -29,7 +38,14 @@ class GameScene extends Phaser.Scene {
             .events.on("joystickMove", this.updatePlayerPosition)
     }
 
-    update = () => {}
+    updateFPS = () => {
+        this.fpsText.setText('FPS: ' + Math.floor(this.game.loop.actualFps));
+    }
+
+
+    update = () => {
+        this.updateFPS()
+    }
 
     addCamera = () => {
         const camera = this.cameras.main
@@ -43,6 +59,11 @@ class GameScene extends Phaser.Scene {
             fill: "#ffffff",
         })
         this.debugText.setScrollFactor(0)
+        this.fpsText = this.add.text(16,16, 'FPS: 0', { 
+            font: '40px Arial', 
+            fill: '#ffffff',
+         })
+        //this.fpsText.setScrollFactor(0)
     }
 
     updatePlayerPosition = (joystick) => {
