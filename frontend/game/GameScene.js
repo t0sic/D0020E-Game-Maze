35b77c268a5a0b4279eb1d85564fb52fd72a3a50
Advokado1337 <Advokado1337>
2024-01-16 14:31:50 +0100

    created tileset, implemented tilemap into phaser, still working on collission

--- frontend/game/GameScene.js
+++ frontend/game/GameScene.js
@@ -14,6 +14,32 @@ class GameScene extends Phaser.Scene {
     init = () => {
         console.log("GameScene init")
     }
+
+    preload = () => {
+        this.load.image("tiles", "/assets/dungeon_tiles.png")
+        this.load.tilemapTiledJSON("dungeon_tiles", "/assets/Tilemap1.json")
+        console.log("GameScene preload")
+    }
+
+    create = () => {
+        console.log("GameScene create")
+        const map = this.make.tilemap({ key: "dungeon_tiles" })
+        const tileset = map.addTilesetImage("dungeon_tiles", "tiles")
+        const groundLayer = map.createLayer("Ground", tileset)
+        const wallLayer = map.createLayer("Walls", tileset)
+
+        wallLayer.setCollisionByProperty({ collides: true })
+
+        const debugGraphics = this.add.graphics().setAlpha(0.7)
+        wallLayer.renderDebug(debugGraphics, {
+            tileColor: null,
+            collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
+            faceColor: new Phaser.Display.Color(40, 39, 37, 255),
+
+            
+        })
+        this.add.existing(debugGraphics);
+    }
 }
 
 export default GameScene
