d8f1ae27696f63b9b9b571478bcb31ef48ad1a36
Fredrik Sundh <101127364+FredrikSundh@users.noreply.github.com>
2024-02-14 14:30:41 +0100

    Made stun always available on a cooldown

--- frontend/game/UIScene.js
+++ frontend/game/UIScene.js
@@ -50,27 +50,64 @@ class UIScene extends Phaser.Scene {
             this[type + "Button"] = this.add.container(offsetX, offsetY)
             const image = this.add.image(0, 0, type + "_button")
             image.setScale(8)
+            console.log("logging spelltype in uiscene", type)
 
             this[type + "Button"].add([image])
             // Add hit area to container
             this[type + "Button"].setSize(100, 100)
             this[type + "Button"].setInteractive()
+
             this[type + "Button"].on("pointerdown", () => {
                 if (this[type + "Button"].alpha !== 1) return
                 this[type + "Button"].setAlpha(0.5)
+                if (type === "stun") {
+                    const cooldownOverlay = this.setupCooldownOverlay()
+                    this.triggerCooldown(cooldownOverlay)
+                }
                 eventEmitter.emit("onSpellButtonClicked", type)
             })
         })
 
         this.updateSpellButtons([])
     }
+    setupCooldownOverlay = () => {
+        console.log("In cooldownSetup")
+        const cooldownOverlay = this.add.graphics()
+        cooldownOverlay.setAlpha(0)
+        console.log("logging container", this.stunButton)
+        this.stunButton.setAlpha(1)
+        cooldownOverlay.fillStyle(0x0000ff, 0.5) // Black with 50% transparency
+        cooldownOverlay.fillRect(0 - 50, 0 - 50, 100, 100) // Draw a square shape (100x100)
+        console.log(cooldownOverlay)
+        this.stunButton.add(cooldownOverlay)
+        return cooldownOverlay
+    }
+
+    triggerCooldown = (cooldownOverlay) => {
+        this.stunButton.setAlpha(0.5)
+        cooldownOverlay.setAlpha(1)
+        const cooldownTween = this.tweens.add({
+            targets: cooldownOverlay,
+            scaleY: 0, // Shrink the height to 0
+            y: 50,
+            duration: 1000, // Duration of the cooldown effect (5000 milliseconds = 5 seconds)
+            transformOrigin: { x: 0, y: 0 }, // Set the transform origin to the top-left corner
+            onComplete: () => {
+                this.stunButton.setAlpha(1)
+                cooldownOverlay.setAlpha(0)
+            },
+        })
+    }
 
     updateSpellButtons = (spells) => {
         this.spellTypes.forEach((spell) => {
-            if (spells.includes(spell)) {
-                this[spell + "Button"].setAlpha(1)
-            } else {
-                this[spell + "Button"].setAlpha(0.5)
+            if (spell != "stun") {
+                console.log("this is not a stun", spell)
+                if (spells.includes(spell)) {
+                    this[spell + "Button"].setAlpha(1)
+                } else {
+                    this[spell + "Button"].setAlpha(0.5)
+                }
             }
         })
     }
