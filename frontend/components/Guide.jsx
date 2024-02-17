12ef20b4f825e6379c4f99fb06de2da4df23118e
Fredrik Sundh <101127364+FredrikSundh@users.noreply.github.com>
2024-02-17 16:05:11 +0100

    guide page mostly finished, added gifs, images and spell description

--- frontend/components/Guide.jsx
+++ frontend/components/Guide.jsx
@@ -3,6 +3,12 @@ import "../styles/Guide.css"
 import key from "/public/assets/pickups/key.png"
 import SpriteAnimation from "./utility/SpriteAnimation"
 import door from "/public/assets/ui/door.png"
+import joystickControls from "/public/assets/ui/joystick-controls.gif"
+import spellcastGif from "/public/assets/ui/spellcastGif.gif"
+import stun from "/public/assets/ui/stun.png"
+import slow from "/public/assets/ui/slow.png"
+import confuse from "/public/assets/ui/confuse.png"
+import haste from "/public/assets/ui/haste.png"
 
 const Guide = () => {
     return (
@@ -37,8 +43,63 @@ const Guide = () => {
                         <img src={door} width={"135px"}></img>
                     </div>
                 </div>
+                <div className="controls">
+                    <h1 style={{ textAlign: "center" }}>Controls</h1>
+                    <p>
+                        To move your character simply drag the joystick on the
+                        screen to go in the desired direction
+                    </p>
+                    <img
+                        className="gif joystickControlGif"
+                        src={joystickControls}
+                    ></img>
+                    <h2>Use your spells</h2>
+                    <p>
+                        As a wizard you are nothing without your spells, touch a
+                        spell icon to cast a spell
+                    </p>
+                    <img className="gif spellcastGif" src={spellcastGif}></img>
+                    <p>
+                        Here you can see a wizard casting the stun spell. This
+                        spell is always always available on a short cooldown,
+                        Hitting your opponent with this spell will make them
+                        drop the key if they have it!
+                    </p>
+                    <div className="spell-explanationContainer">
+                        <div className="spellinfo spellimage stun">
+                            <h5>Stun</h5>
+                            <img src={stun} style={{ width: "5rem" }}></img>
+                            <p>
+                                Stuns the opponent for a few seconds as well as
+                                makes them drop the key if they have it. Always
+                                available on a short cooldown
+                            </p>
+                        </div>
+                        <div className="spellinfo spellimage slow">
+                            <h5>Slow</h5>
+                            <img src={slow} style={{ width: "5rem" }}></img>
+                            <p>
+                                The slow spell is acquired by picking it up on
+                                the map it will greatly slow your opponent
+                            </p>
+                        </div>
+                        <div className="spellinfo spellimage confuse">
+                            <h5>confuse</h5>
+                            <img src={confuse} style={{ width: "5rem" }}></img>
+                            <p>
+                                The confuse spells invers the opponents controls
+                                for a few seconds
+                            </p>
+                        </div>
+                        <div className="spellinfo spellimage haste">
+                            <h5>haste</h5>
+                            <img src={haste} style={{ width: "5rem" }}></img>
 
-                <h1 style={{ textAlign: "center" }}>Controls</h1>
+                            <p>The haste spell makes you incredibly fast</p>
+                        </div>
+                    </div>
+                    {/*This container will have all spell icons and explanations */}
+                </div>
             </div>
             <div className="textbox">
                 <div className="header">Controlls</div>
