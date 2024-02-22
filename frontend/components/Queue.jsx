f0480e40b285f93180136fd396de428093383972
t0sic <kanieckimikolaj@gmail.com>
2024-02-22 13:03:24 +0100

    Added rotate warning

--- frontend/components/Queue.jsx
+++ frontend/components/Queue.jsx
@@ -1,8 +1,10 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "../styles/Queue.css"
 
 const Queue = ({ queueState, onLeave }) => {
     let stateText
+
+    const [rotate, setRotate] = useState(window.innerWidth < window.innerHeight)
 
     switch (queueState) {
         case "connecting":
@@ -18,12 +20,33 @@ const Queue = ({ queueState, onLeave }) => {
             stateText = "Session ended"
     }
 
+    useEffect(() => {
+        window.addEventListener("resize", () => {
+            if (window.innerWidth < window.innerHeight) {
+                setRotate(true)
+            } else {
+                setRotate(false)
+            }
+        })
+    }, [])
+
     return (
         <div
             className="queue"
             style={{ background: "url(/assets/ui/background3.png)" }}
         >
             <div className="queue-container">
+                {rotate && (
+                    <div className="queue-rotate-container">
+                        <div>Rotate Your Device</div>
+                        <div
+                            className="queue-rotate"
+                            style={{
+                                backgroundImage: "url(/assets/ui/rotate.png)",
+                            }}
+                        ></div>
+                    </div>
+                )}
                 <div className="state">{stateText}...</div>
                 <button onClick={onLeave} className="button">
                     Leave
